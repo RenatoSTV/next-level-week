ba4a8e6bf389f612a064848903e438e588bb290a
Renato ST <renatostv@gmail.com>
2020-06-04 21:07:10 -0300

    Quarto dia do NLW
    - Renomeação da pasta do projeto
    
    - Inicio da criação e configuração do servidor (criação e organização de pastas)
    
    - Implementação de tamplate engine através do nunjucks
    
    - Alterações para responsividade em algumas resoluções

---
+++ NextLevelWeek/public/scripts/create-point.js
@@ -0,0 +1,104 @@
+
+// Estados e Cidades
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+        .then(res => res.json())
+        .then(states => {
+
+            for (const state of states) {
+                ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+            }
+
+
+        })
+}
+
+populateUFs()
+
+function getCities(event) {
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    
+
+    
+    fetch(url)
+        .then(res => res.json())
+        .then(cities => {
+            citySelect.innerHTML = `<option value="">Seleciona a Cidade</option>`
+            for (const city of cities) {
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+            }
+
+
+            citySelect.disabled = false
+        })
+}
+
+
+document
+    .querySelector("select[name=uf")
+    .addEventListener("change", getCities) 
+
+// Itens de coleta
+// pegar todos os li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for(const item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items")
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+    const itemLi = event.target
+
+    //adicionar ou remover uma classe com javascript
+    itemLi.classList.toggle("selected")
+
+    const itemId = event.target.dataset.id  
+
+    
+
+    // verificar se exitem itens selecionados, se sim
+    //pegaros itens selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item == itemId //isso será true ou false
+        return itemFound
+    })
+
+    //se ja estiver selecionado,
+
+    if (alreadySelected >= 0){
+        //tirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId // false
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems
+    } else{
+        //se não estiver selecionado
+        //adicionar à seleceção
+        selectedItems.push(itemId)
+    }
+
+    //atualizar o campo escondido com os itens selecionados
+    collectedItems.value = selectedItems
+
+}
